f0a4bd65997a9fe74b614be1f1c7d3a35d15724f
mikesocorec <54631530+mikesocorec@users.noreply.github.com>
2020-01-21 15:09:03 -0600

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,27 @@
+<?php
+        // Declaracion de variables
+        $nombre = $_POST['nombre'];
+        $telefono = $_POST['telefono'];
+        $correo = $_POST['correo'];
+        $mensaje = $_POST['mensaje']; 
+        $noticia = $_POST['noticia'];
+        $email = 'mikesocorec@gmail.com';
+        $para = $email;
+        
+        $titulo = 'Solicitud de contacto';
+        $header = 'From: '. $email;
+        $msjCorreo = "Nombre: $nombre\n Telefono: $telefono\n Correo: $email Mensaje: $mensaje Noticia: $noticia";
+        if ($_POST['enviar']){
+                if(mail($para, $titulo, $msjCorreo, $header)){
+                        echo "<script>
+                        alert('mensaje Enviado, muchas gracias por contactar con nosotros.');
+                        </script>";
+                } else{
+                        echo "<script>
+                        alert('Fallo el envio');
+                        </script>";
+                }
+        }
+
+    
+?>
